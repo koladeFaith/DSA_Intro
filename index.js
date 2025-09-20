43360e9ec4ae6b7f73fe60634edd3c763762ef93
Kolade Faith <faithkolade0507@gmail.com>
2025-09-20 16:14:50 +0100

    Work done

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const food = "Rice and beans"
 console.log(food);
 console.timeEnd('variable')
 console.log("yyyyy");
+console.time("variable")
+const food2 = "Rice and beans"
+console.log(food2);
+console.timeEnd('variable')
